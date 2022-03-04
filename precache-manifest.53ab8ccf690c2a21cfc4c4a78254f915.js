fb8a39b2536d1bfcf7972353105bea6e7b00f313
ft457 <ideagood64@gmail.com>
2022-03-04 08:45:33 -0500

    Updates

---
+++ precache-manifest.53ab8ccf690c2a21cfc4c4a78254f915.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb14d53e8a6d4585adea0420f271c229",
+    "url": "/rpn-react/index.html"
+  },
+  {
+    "revision": "0163cae9c87315e1be60",
+    "url": "/rpn-react/static/css/main.d7e07d52.chunk.css"
+  },
+  {
+    "revision": "5e5eebc2189041f4510c",
+    "url": "/rpn-react/static/js/2.d12e5895.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/rpn-react/static/js/2.d12e5895.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0163cae9c87315e1be60",
+    "url": "/rpn-react/static/js/main.d7d43639.chunk.js"
+  },
+  {
+    "revision": "7dc9122616c909b20a2d",
+    "url": "/rpn-react/static/js/runtime-main.f4aa33df.js"
+  }
+]);
